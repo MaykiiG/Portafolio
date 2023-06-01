32c617e86a045af6647ef11848b5c841d20360e3
Maycol <prueba@correo.com>
2023-05-31 21:41:26 -0500

    first commit

---
+++ javaScript/main.js
@@ -0,0 +1,16 @@
+const typed = new Typed('.typed',{
+    strings:['<i class="tituloo">Hola soy Maycol Gutierrez</i>','<i class="tituloo">Desarrallador web</i>',
+]
+,
+typeSpeed:70,
+startDelay:100,
+backSpeed:75,
+backDelay:1500,
+loop:true,
+loopCount:false,
+cursorChar:' ☺'
+
+ }
+
+
+);
